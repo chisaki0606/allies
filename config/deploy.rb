9edc0a5a268e01d5da4ac77cad0e1c44611a4cf9
“chisaki0606” <witness6961@gmail.com>
2020-12-05 00:48:12 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<Capistranoのバージョン>'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'allies'
@@ -18,7 +18,7 @@ set :repo_url,  'git@github.com:chisaki0606/allies.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<このアプリで使用しているrubyのバージョン>' 
+set :rbenv_ruby, '2.5.8'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
