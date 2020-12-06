45584b02faedc96d8f9a98144e001667bd75e959
“chisaki0606” <witness6961@gmail.com>
2020-12-06 14:06:28 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ lock '3.14.1'
 # Capistranoのログの表示に利用する
 set :application, 'allies'
 
+set :stage,           :production
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
